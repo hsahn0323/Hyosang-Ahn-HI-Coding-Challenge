1054e9e29e363208c77a979e68d1cb4d11e0ed74
hsahn0323 <hyosang2@illinois.edu>
2020-09-10 15:26:56 -0500

    Initial Commit

---
+++ Hyosang Ahn HI Coding Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hyosang Ahn HI Coding Challenge
+//
+//  Created by Hyosang Ahn on 9/10/20.
+//  Copyright © 2020 Hyosang Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
